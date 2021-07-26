LineageOS/android_packages_apps_SetupWizard

Switch fingerprint setup to biometric setup instead
* Generic, handles both face and fingerprint
* Works the same as before for fingerprint
* Works the same as fingerprint for face
* What else could one ask for?

Change-Id: I9ddd7b46548c8107239fdec457bbaae6b12876e6

src/org/lineageos/setupwizard/BiometricActivity.java
@@ -17,7 +17,7 @@
 
 package org.lineageos.setupwizard;
 
-import static org.lineageos.setupwizard.SetupWizardApp.ACTION_SETUP_FINGERPRINT;
+import static org.lineageos.setupwizard.SetupWizardApp.ACTION_SETUP_BIOMETRIC;
 import static org.lineageos.setupwizard.SetupWizardApp.EXTRA_ALLOW_SKIP;
 import static org.lineageos.setupwizard.SetupWizardApp.EXTRA_AUTO_FINISH;
 import static org.lineageos.setupwizard.SetupWizardApp.EXTRA_DETAILS;
@@ -26,49 +26,52 @@ import static org.lineageos.setupwizard.SetupWizardApp.EXTRA_MATERIAL_LIGHT;
 import static org.lineageos.setupwizard.SetupWizardApp.EXTRA_THEME;
 import static org.lineageos.setupwizard.SetupWizardApp.EXTRA_TITLE;
 import static org.lineageos.setupwizard.SetupWizardApp.EXTRA_USE_IMMERSIVE;
-import static org.lineageos.setupwizard.SetupWizardApp.REQUEST_CODE_SETUP_FINGERPRINT;
+import static org.lineageos.setupwizard.SetupWizardApp.REQUEST_CODE_SETUP_BIOMETRIC;
 
 import android.content.Intent;
 import android.view.View;
 
-public class FingerprintActivity extends SubBaseActivity {
+import com.google.android.setupcompat.util.WizardManagerHelper;
 
-    public static final String TAG = FingerprintActivity.class.getSimpleName();
+public class BiometricActivity extends SubBaseActivity {
+
+    public static final String TAG = BiometricActivity.class.getSimpleName();
 
     @Override
     protected void onStartSubactivity() {
         setNextAllowed(true);
-        findViewById(R.id.setup_fingerprint).setOnClickListener(view -> launchFingerprintSetup());
+        findViewById(R.id.setup_biometric).setOnClickListener(view -> launchBiometricSetup());
     }
 
     @Override
     protected int getLayoutResId() {
-        return R.layout.setup_fingerprint;
+        return R.layout.setup_biometric;
     }
 
     @Override
     protected int getTitleResId() {
-        return R.string.fingerprint_setup_title;
+        return R.string.biometric_setup_title;
     }
 
     @Override
     protected int getIconResId() {
-        return R.drawable.ic_fingerprint;
+        return R.drawable.ic_fingerprint; // TODO?
     }
 
-    private void launchFingerprintSetup() {
-        Intent intent = new Intent(ACTION_SETUP_FINGERPRINT);
+    private void launchBiometricSetup() {
+        Intent intent = new Intent(ACTION_SETUP_BIOMETRIC);
         intent.putExtra(EXTRA_FIRST_RUN, true);
         intent.putExtra(EXTRA_ALLOW_SKIP, true);
         intent.putExtra(EXTRA_USE_IMMERSIVE, true);
         intent.putExtra(EXTRA_THEME, EXTRA_MATERIAL_LIGHT);
         intent.putExtra(EXTRA_AUTO_FINISH, false);
-            /*intent.putExtra(LockPatternUtils.LOCKSCREEN_FINGERPRINT_FALLBACK, true);*/
+            /*intent.putExtra(LockPatternUtils.LOCKSCREEN_BIOMETRIC_FALLBACK, true);*/
         intent.putExtra(EXTRA_TITLE,
-                getString(R.string.settings_fingerprint_setup_title));
+                getString(R.string.settings_biometric_setup_title));
         intent.putExtra(EXTRA_DETAILS,
-                getString(R.string.settings_fingerprint_setup_details));
-        startSubactivity(intent, REQUEST_CODE_SETUP_FINGERPRINT);
+                getString(R.string.settings_biometric_setup_details));
+        intent.putExtra(WizardManagerHelper.EXTRA_IS_SETUP_FLOW, true);
+        startSubactivity(intent, REQUEST_CODE_SETUP_BIOMETRIC);
     }
 
     @Override

src/org/lineageos/setupwizard/util/SetupWizardUtils.java
@@ -42,6 +42,7 @@ import android.content.pm.ComponentInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ServiceInfo;
+import android.hardware.face.FaceManager;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Binder;
 import android.os.SystemProperties;
@@ -55,7 +56,7 @@ import android.util.Log;
 
 import org.lineageos.setupwizard.BluetoothSetupActivity;
 import org.lineageos.setupwizard.ChooseDataSimActivity;
-import org.lineageos.setupwizard.FingerprintActivity;
+import org.lineageos.setupwizard.BiometricActivity;
 import org.lineageos.setupwizard.MobileDataActivity;
 import org.lineageos.setupwizard.SetupWizardApp;
 import org.lineageos.setupwizard.SimMissingActivity;
@@ -224,12 +225,16 @@ public class SetupWizardUtils {
         return packageManager.hasSystemFeature(PackageManager.FEATURE_LEANBACK);
     }
 
-    public static boolean hasFingerprint(Context context) {
+    public static boolean hasBiometric(Context context) {
         PackageManager packageManager = context.getPackageManager();
         if (packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)) {
             FingerprintManager fingerprintManager = (FingerprintManager)
                     context.getSystemService(Context.FINGERPRINT_SERVICE);
             return fingerprintManager.isHardwareDetected();
+        } else if (packageManager.hasSystemFeature(PackageManager.FEATURE_FACE)) {
+            FaceManager faceManager = (FaceManager)
+                    context.getSystemService(Context.FACE_SERVICE);
+            return faceManager.isHardwareDetected();
         } else {
             return false;
         }
@@ -243,8 +248,8 @@ public class SetupWizardUtils {
         if (!hasLeanback(context)) {
             disableComponent(context, BluetoothSetupActivity.class);
         }
-        if (!hasFingerprint(context)) {
-            disableComponent(context, FingerprintActivity.class);
+        if (!hasBiometric(context)) {
+            disableComponent(context, BiometricActivity.class);
         }
         if (!hasTelephony(context)) {
             disableComponent(context, MobileDataActivity.class);

CarbonROM/android_packages_apps_CarbonSetupWizard

src/org/carbonrom/setupwizard/BiometricActivity.java
@@ -17,7 +17,7 @@
 
 package org.carbonrom.setupwizard;
 
-import static org.carbonrom.setupwizard.SetupWizardApp.ACTION_SETUP_FINGERPRINT;
+import static org.carbonrom.setupwizard.SetupWizardApp.ACTION_SETUP_BIOMETRIC;
 import static org.carbonrom.setupwizard.SetupWizardApp.EXTRA_ALLOW_SKIP;
 import static org.carbonrom.setupwizard.SetupWizardApp.EXTRA_AUTO_FINISH;
 import static org.carbonrom.setupwizard.SetupWizardApp.EXTRA_DETAILS;
@@ -26,48 +26,75 @@ import static org.carbonrom.setupwizard.SetupWizardApp.EXTRA_MATERIAL_LIGHT;
 import static org.carbonrom.setupwizard.SetupWizardApp.EXTRA_THEME;
 import static org.carbonrom.setupwizard.SetupWizardApp.EXTRA_TITLE;
 import static org.carbonrom.setupwizard.SetupWizardApp.EXTRA_USE_IMMERSIVE;
-import static org.carbonrom.setupwizard.SetupWizardApp.REQUEST_CODE_SETUP_FINGERPRINT;
+import static org.carbonrom.setupwizard.SetupWizardApp.REQUEST_CODE_SETUP_BIOMETRIC;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-public class FingerprintActivity extends SubBaseActivity {
+import com.google.android.setupcompat.util.WizardManagerHelper;
 
-    public static final String TAG = FingerprintActivity.class.getSimpleName();
+import org.carbonrom.setupwizard.util.SetupWizardUtils;
+
+public class BiometricActivity extends SubBaseActivity {
+
+    public static final String TAG = BiometricActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final TextView setupBiometricSummary = (TextView) findViewById(R.id.setup_biometric_summary);
+        final TextView setupAddBiometric = (TextView) findViewById(R.id.setup_add_biometric);
+        if (SetupWizardUtils.hasFace(this)) {
+            setupBiometricSummary.setText(getString(R.string.face_setup_summary));
+            setupAddBiometric.setText(R.string.face_setup_add_face);
+        } else {
+            setupBiometricSummary.setText(getString(R.string.fingerprint_setup_summary));
+            setupAddBiometric.setText(R.string.fingerprint_setup_add_fingerprint);
+        }
+    }
 
     @Override
     protected void onStartSubactivity() {
         setNextAllowed(true);
-        findViewById(R.id.setup_fingerprint).setOnClickListener(view -> launchFingerprintSetup());
+        findViewById(R.id.setup_biometric).setOnClickListener(view -> launchBiometricSetup());
     }
 
     @Override
     protected int getLayoutResId() {
-        return R.layout.setup_fingerprint;
+        return R.layout.setup_biometric;
     }
 
     @Override
     protected int getTitleResId() {
+        if (SetupWizardUtils.hasFace(this)) {
+            return R.string.face_setup_title;
+        }
         return R.string.fingerprint_setup_title;
     }
 
     @Override
     protected int getIconResId() {
+        if (SetupWizardUtils.hasFace(this)) {
+            return R.drawable.ic_face;
+        }
         return R.drawable.ic_fingerprint;
     }
 
-    private void launchFingerprintSetup() {
-        Intent intent = new Intent(ACTION_SETUP_FINGERPRINT);
+    private void launchBiometricSetup() {
+        Intent intent = new Intent(ACTION_SETUP_BIOMETRIC);
         intent.putExtra(EXTRA_FIRST_RUN, true);
         intent.putExtra(EXTRA_ALLOW_SKIP, true);
         intent.putExtra(EXTRA_USE_IMMERSIVE, true);
         intent.putExtra(EXTRA_THEME, EXTRA_MATERIAL_LIGHT);
         intent.putExtra(EXTRA_AUTO_FINISH, false);
-            /*intent.putExtra(LockPatternUtils.LOCKSCREEN_FINGERPRINT_FALLBACK, true);*/
+            /*intent.putExtra(LockPatternUtils.LOCKSCREEN_BIOMETRIC_FALLBACK, true);*/
         intent.putExtra(EXTRA_TITLE,
-                getString(R.string.settings_fingerprint_setup_title));
+                getString(getTitleResId()));
         intent.putExtra(EXTRA_DETAILS,
-                getString(R.string.settings_fingerprint_setup_details));
-        startSubactivity(intent, REQUEST_CODE_SETUP_FINGERPRINT);
+                getString(R.string.settings_biometric_setup_details));
+        intent.putExtra(WizardManagerHelper.EXTRA_IS_SETUP_FLOW, true);
+        startSubactivity(intent, REQUEST_CODE_SETUP_BIOMETRIC);
     }
 }

src/org/carbonrom/setupwizard/util/SetupWizardUtils.java
@@ -42,6 +42,7 @@ import android.content.pm.ComponentInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ServiceInfo;
+import android.hardware.face.FaceManager;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Binder;
 import android.os.SystemProperties;
@@ -57,7 +58,7 @@ import com.android.internal.util.cr.CrUtils;
 
 import org.carbonrom.setupwizard.BluetoothSetupActivity;
 import org.carbonrom.setupwizard.ChooseDataSimActivity;
-import org.carbonrom.setupwizard.FingerprintActivity;
+import org.carbonrom.setupwizard.BiometricActivity;
 import org.carbonrom.setupwizard.MobileDataActivity;
 import org.carbonrom.setupwizard.SetupWizardApp;
 import org.carbonrom.setupwizard.SimMissingActivity;
@@ -228,12 +229,27 @@ public class SetupWizardUtils {
         return packageManager.hasSystemFeature(PackageManager.FEATURE_LEANBACK);
     }
 
+    public static boolean hasBiometric(Context context) {
+        return hasFingerprint(context) || hasFace(context);
+    }
+
     public static boolean hasFingerprint(Context context) {
         PackageManager packageManager = context.getPackageManager();
         if (packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)) {
             FingerprintManager fingerprintManager = (FingerprintManager)
                     context.getSystemService(Context.FINGERPRINT_SERVICE);
             return fingerprintManager.isHardwareDetected();
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean hasFace(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        if (packageManager.hasSystemFeature(PackageManager.FEATURE_FACE)) {
+            FaceManager faceManager = (FaceManager)
+                    context.getSystemService(Context.FACE_SERVICE);
+            return faceManager.isHardwareDetected();
         } else {
             return false;
         }
@@ -247,8 +263,8 @@ public class SetupWizardUtils {
         if (!hasLeanback(context)) {
             disableComponent(context, BluetoothSetupActivity.class);
         }
-        if (!hasFingerprint(context)) {
-            disableComponent(context, FingerprintActivity.class);
+        if (!hasBiometric(context)) {
+            disableComponent(context, BiometricActivity.class);
         }
         if (!hasTelephony(context)) {
             disableComponent(context, MobileDataActivity.class);