LineageOS/android_packages_apps_SetupWizard

SetupWizard: Distinguish interactive and wrapper subactivities
We were using the SubBaseActivity class for two different kind of
activities:
 (1) "Wrapper" activities, where we define an activity that on
     start simply launches another one
 (2) "Interactive" activities, where the user has to manually
     trigger the start of the subactivity, or skip the step.

When the subactivity ends, only in case (1) we want to finish our
activity as well, since we wouldn't have anything to show.

Change-Id: I1a3ae51f6146ac32b5e7542d9a18b0b032efe144

src/org/lineageos/setupwizard/WrapperSubBaseActivity.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.setupwizard;
+
+import android.content.Intent;
+
+public abstract class WrapperSubBaseActivity extends SubBaseActivity {
+
+    @Override
+    protected void onSubactivityCanceled(Intent data) {
+        super.onSubactivityCanceled(data);
+
+        // As the subactivity we're wrapping finishes, we finish too
+        applyBackwardTransition(getSubactivityPreviousTransition());
+        finishAction(RESULT_CANCELED, data);
+    }
+
+}

CarbonROM/android_packages_apps_CarbonSetupWizard

src/org/carbonrom/setupwizard/WrapperSubBaseActivity.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.carbonrom.setupwizard;
+
+import android.content.Intent;
+
+public abstract class WrapperSubBaseActivity extends SubBaseActivity {
+
+    @Override
+    protected void onSubactivityCanceled(Intent data) {
+        super.onSubactivityCanceled(data);
+
+        // As the subactivity we're wrapping finishes, we finish too
+        applyBackwardTransition(getSubactivityPreviousTransition());
+        finishAction(RESULT_CANCELED, data);
+    }
+
+}