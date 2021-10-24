LineageOS/android_packages_apps_SetupWizard

Changes for new network setup flow
* Remove mobile data/choose data sim pages
* Rename Wi-Fi setup to network setup
* Switch to the new NetworkProviderSettings which contains
  both Wi-Fi and mobile data setup.

Change-Id: I1c618d629628740e39dbed313d468d1efe971ed7

src/org/lineageos/setupwizard/util/SetupWizardUtils.java
@@ -55,11 +55,9 @@ import android.util.Log;
 import org.lineageos.internal.util.PackageManagerUtils;
 import org.lineageos.setupwizard.BiometricActivity;
 import org.lineageos.setupwizard.BluetoothSetupActivity;
-import org.lineageos.setupwizard.ChooseDataSimActivity;
-import org.lineageos.setupwizard.MobileDataActivity;
+import org.lineageos.setupwizard.NetworkSetupActivity;
 import org.lineageos.setupwizard.SetupWizardApp;
 import org.lineageos.setupwizard.SimMissingActivity;
-import org.lineageos.setupwizard.WifiSetupActivity;
 import org.lineageos.setupwizard.wizardmanager.WizardManager;
 
 import java.io.File;
@@ -296,18 +294,11 @@ public class SetupWizardUtils {
         if (!hasBiometric(context)) {
             disableComponent(context, BiometricActivity.class);
         }
-        if (!hasTelephony(context)) {
-            disableComponent(context, MobileDataActivity.class);
-            disableComponent(context, SimMissingActivity.class);
-            disableComponent(context, ChooseDataSimActivity.class);
-        } else if (!simMissing()) {
+        if (!hasTelephony(context) || !simMissing()) {
             disableComponent(context, SimMissingActivity.class);
         }
-        if (!isMultiSimDevice() || singleSimInserted()) {
-            disableComponent(context, ChooseDataSimActivity.class);
-        }
-        if (!hasWifi(context) || isEthernetConnected(context)) {
-            disableComponent(context, WifiSetupActivity.class);
+        if ((!hasWifi(context) && !hasTelephony(context)) || isEthernetConnected(context)) {
+            disableComponent(context, NetworkSetupActivity.class);
         }
     }
 

CarbonROM/android_packages_apps_CarbonSetupWizard

src/org/carbonrom/setupwizard/util/SetupWizardUtils.java
@@ -56,13 +56,11 @@ import android.util.Log;
 
 import com.android.internal.util.cr.CrUtils;
 
-import org.carbonrom.setupwizard.BluetoothSetupActivity;
-import org.carbonrom.setupwizard.ChooseDataSimActivity;
 import org.carbonrom.setupwizard.BiometricActivity;
-import org.carbonrom.setupwizard.MobileDataActivity;
+import org.carbonrom.setupwizard.BluetoothSetupActivity;
+import org.carbonrom.setupwizard.NetworkSetupActivity;
 import org.carbonrom.setupwizard.SetupWizardApp;
 import org.carbonrom.setupwizard.SimMissingActivity;
-import org.carbonrom.setupwizard.WifiSetupActivity;
 import org.carbonrom.setupwizard.wizardmanager.WizardManager;
 
 import java.util.ArrayList;
@@ -266,20 +264,11 @@ public class SetupWizardUtils {
         if (!hasBiometric(context)) {
             disableComponent(context, BiometricActivity.class);
         }
-        if (!hasTelephony(context)) {
-            disableComponent(context, MobileDataActivity.class);
+        if (!hasTelephony(context) || !simMissing()) {
             disableComponent(context, SimMissingActivity.class);
-            disableComponent(context, ChooseDataSimActivity.class);
         }
-        if (!SetupWizardUtils.isMultiSimDevice(context)) {
-            disableComponent(context, ChooseDataSimActivity.class);
-        } else if (simMissing()) {
-            disableComponent(context, MobileDataActivity.class);
-            disableComponent(context, ChooseDataSimActivity.class);
-        }
-        if (!SetupWizardUtils.hasWifi(context) ||
-            isEthernetConnected(context)) {
-            disableComponent(context, WifiSetupActivity.class);
+        if ((!hasWifi(context) && !hasTelephony(context)) || isEthernetConnected(context)) {
+            disableComponent(context, NetworkSetupActivity.class);
         }
     }
 